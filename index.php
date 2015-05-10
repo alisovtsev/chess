ddd1b0cbceb0a770495c2d26523fe52c82b99832
alisovtsev <alisovtsev@gmail.com>
2015-05-10 14:25:49 +0600

    fixed save actions

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<?php
+session_start();
+$figuresDefault = array(
+    1 => array(
+        'img' => 'img/ladyablack.png',
+        'x' => 1,
+        'y' => 1,
+    ),
+    2 => array(
+        'img' => 'img/ferzwhite.png',
+        'x' => 3,
+        'y' => 1,
+    )
+);
+if (empty($_SESSION['figureSelectedId'])) {
+    $_SESSION['figureSelectedId'] = false;
+}
+$pu='img/pustaya.png';
+
+$action = !empty($_GET['action']) ? $_GET['action'] : false;
+$figureSelectedId = !empty($_GET['figureSelectedId']) ? $_GET['figureSelectedId'] : $_SESSION['figureSelectedId'];
+$clickX = !empty($_GET['x']) ? $_GET['x'] : 1;
+$clickY = !empty($_GET['y']) ? $_GET['y'] : 1;
+$figures = !empty($_SESSION['figures']) ? $_SESSION['figures'] : $figuresDefault;
+
+if ($figureSelectedId) {
+    if ($action == 'move') {
+        $figures[$figureSelectedId]['x'] = $clickX;
+        $figures[$figureSelectedId]['y'] = $clickY;
+        $_SESSION['figures'] = $figures;
+        header("Location: /");
+        exit;
+    }
+    if ($action == 'select') {
+        $_SESSION['figureSelectedId'] = $figureSelectedId;
+        header("Location: /");
+        exit;
+    }
+}
+?>
+
 <html>
 <head>
     <title>chess</title>
@@ -5,33 +46,7 @@
     <div align=center>
 </head>
 <body>
-<a href="index.php">nag lavnuju</a>
-<?php
-print_r($_GET);
-$pu='img/pustaya.png';
 
-    $figureSelectedId = !empty($_GET['figureSelectedId']) ? $_GET['figureSelectedId'] : false;
-    $clickX = !empty($_GET['x']) ? $_GET['x'] : 1;
-    $clickY = !empty($_GET['y']) ? $_GET['y'] : 1;
-    $action = !empty($_GET['action']) ? $_GET['action'] : false;
-$figures = array(
-    array(
-        'id' => 1,
-        'img' => 'img/ladyablack.png',
-        'x' => 1,
-        'y' => 1,
-    ),
-    array(
-        'id' => 2,
-        'img' => 'img/ferzwhite.png',
-        'x' => 3,
-        'y' => 1,
-    )
-);
-
-echo "figureSelected= $figureSelectedId<br>
-                                     <br>";
-?>
 <table id="table1" border="1">
   <?php for ($j = 1; $j <= 8; $j++) : ?>
     <tr>
@@ -39,25 +54,19 @@ echo "figureSelected= $figureSelectedId<br>
         <td <?php if (($j%2!=0 && $i%2==0) || ($j%2==0 && $i%2!=0)) echo 'class="grey"' ?>>
           <?php
           $tdFigureExists = false;
-          foreach ($figures as $figure) {
-              if (($action != 'move' || $figureSelectedId != $figure['id']) && $figure['x'] == $i && $figure['y'] == $j) {
+          foreach ($figures as $figureId => $figureData) {
+              if ($figureData['x'] == $i && $figureData['y'] == $j) {
                   ?>
-                  <a href="index.php?action=select&figureSelectedId=<?php echo $figure['id']?>&x=<?php echo $i ?>&y=<?php echo $j ?>">
-                      <div <?php if($clickX == $i && $clickY == $j && $action == 'select')  echo 'class="ramkavokrugfiguri"'?>><img src=<?php echo $figure['img']?>></div>
+                  <a href="index.php?action=select&figureSelectedId=<?php echo $figureId?>&x=<?php echo $i ?>&y=<?php echo $j ?>">
+                      <div <?php if($figureSelectedId == $figureId)  echo 'class="ramkavokrugfiguri"'?>><img src=<?php echo $figureData['img']?>></div>
                   </a>
                   <?php $tdFigureExists = true;
-              } elseif ($action == 'move' && $clickX == $i && $clickY == $j && $figure['id'] == $figureSelectedId) {
-                  ?>
-                  <a href="index.php?action=select&figureSelectedId=<?php echo $figure['id']?>&x=<?php echo $i ?>&y=<?php echo $j ?>">
-                      <div <?php echo 'class="ramkavokrugfiguri"'?>><img src=<?php echo $figure['img']?>></div>
-                  </a>
-                <?php $tdFigureExists = true;
               }
           }
           //если выделена фигура и находимся на клетке на которой нет другой фигуры, то делаем ссылку для перемещения нашей фигуры
           if (!$tdFigureExists && $figureSelectedId) :?>
               <a href="index.php?action=move&figureSelectedId=<?php echo $figureSelectedId?>&x=<?php echo $i ?>&y=<?php echo $j ?>">
-                  <div><img src=<?php echo $pu?>></div>
+                  <div><img class="pu" src=<?php echo $pu?>></div>
               </a>
           <?php endif;?>
         </td>
